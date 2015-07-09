2fe212b6122bf100d1bcec09d2283e137d9ee236
Jim Banks <iamjimhbanks@me.com>
2015-07-09 14:33:44 +0100

    added httparty

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'nokogiri'
 
 gem 'nori'
 
+gem 'httparty'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-    root 'show#index'
+    root 'fish#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
