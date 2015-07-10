1dc278db75df0f099c920240844ab16eee99a31e
Jim Banks <iamjimhbanks@me.com>
2015-07-10 15:18:26 +0100

    ugh, ajax is broken

--- app/assets/javascripts/fish.js
+++ app/assets/javascripts/fish.js
@@ -30,16 +30,10 @@ function getFishes(e) {
   });
 }
 
+results.html()
 
 // end of document.ready
 });
 
 
 
-<% fishname << stuff['Seafood_display_name'] %>
-<% mbarank << stuff['MBA_Rank'] %>
-<% omegalevel << stuff['Omega_Level'] %>
-<% mercuryrank << stuff['Mercury_Rank'] %>
-<% foodchain << stuff['Food_Chain'] %>
-<% mercuryppm << stuff['Mercury_PPM']%>
-<% sustainabiltytext << stuff['Sustainability_Seafood_Box_Text'] %>
