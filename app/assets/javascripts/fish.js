96f1cf0c32576de192fb97da0df0a914d3cb3ea8
Jim Banks <iamjimhbanks@me.com>
2015-07-10 14:31:57 +0100

    adding click fucntions

--- app/assets/javascripts/fish.js
+++ app/assets/javascripts/fish.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
 
 var getFishes = $('#get-fishes');
+var getMba = $('#MBA_Rank')
 var results = $('#results');
 
-
-
+// adding an event listener ti get the fishes button on click
+getFishesButton.on('click', getFishes);
+// adding even listener to the the mba rank from data.json, which will change whe  the fish type is changed
+fishSelect.on('change', getMba);
 
 });
 
