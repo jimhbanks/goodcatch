4f8810c6222bd6e9acd69c804493bc8a6ca46131
Jim Banks <iamjimhbanks@me.com>
2015-07-10 14:41:04 +0100

    completed getfishes function

--- app/assets/javascripts/fish.js
+++ app/assets/javascripts/fish.js
@@ -9,7 +9,18 @@ getFishesButton.on('click', getFishes);
 // adding even listener to the the mba rank from data.json, which will change whe  the fish type is changed
 fishSelect.on('change', getMba);
 
+function getFishes(e) {
+  $.ajax({
+    type: "GET",
+    url: "app/db/data.json"
+  }) // ends once done
+  .done(function(response){
+    //emptys the fish everytime so it does not duplicate
+    fishSelect.empty();
 
+  var regions = [];
+  })
+}
 
 
 // end of document.ready
