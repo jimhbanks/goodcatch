66ab2d2dc0d000c5d007b1c1e8aad347e27c5f7e
Jim Banks <iamjimhbanks@me.com>
2015-07-10 11:53:11 +0100

    started on event listener ajax

--- app/assets/javascripts/fish.js
+++ app/assets/javascripts/fish.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
 
+var getFishes = $('#get-fishes');
+var results = $('#results');
+
+
+
+
+});
+
+
+
+<% fishname << stuff['Seafood_display_name'] %>
+<% mbarank << stuff['MBA_Rank'] %>
+<% omegalevel << stuff['Omega_Level'] %>
+<% mercuryrank << stuff['Mercury_Rank'] %>
+<% foodchain << stuff['Food_Chain'] %>
+<% mercuryppm << stuff['Mercury_PPM']%>
+<% sustainabiltytext << stuff['Sustainability_Seafood_Box_Text'] %>
