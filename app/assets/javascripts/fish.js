5b74215f0e680c1b21366fcecdc2986c1b0d05b5
Jim Banks <iamjimhbanks@me.com>
2015-07-13 09:15:39 +0100

    adding push to page

--- app/assets/javascripts/fish.js
+++ app/assets/javascripts/fish.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   console.log('fish.js responding');
-
+var getFishes = $('#get-fishes');
 // var getFishes = $('#get-fishes');
 // // var getMba = $('#MBA_Rank')
 // var results = $('#results');
 
 // adding an event listener ti get the fishes button on click
-// getFishesButton.on('click', getFishes);
+getFishesButton.on('click', getFishes);
 // adding even listener to the the mba rank from data.json, which will change whe  the fish type is changed
 // fishSelect.on('change', getMba);
 
@@ -35,6 +35,8 @@ $(".fishname").on('change', function(){
   getFishes()
 
 })
+
+
 results.html()
 
 // end of document.ready
