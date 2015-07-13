1d62e546394a5e37ecf36a0dba386b2e3c671cb8
Jim Banks <iamjimhbanks@me.com>
2015-07-13 10:27:17 +0100

    adds each fish on

--- app/assets/javascripts/fish.js
+++ app/assets/javascripts/fish.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   console.log('fish.js responding');
-var getFishes = $('#get-fishes');
+// var getFishes = $('#get-fishes');
 // var getFishes = $('#get-fishes');
 // // var getMba = $('#MBA_Rank')
 // var results = $('#results');
 
 // adding an event listener ti get the fishes button on click
-getFishesButton.on('click', getFishes);
+// getFishesButton.on('click', getFishes);
 // adding even listener to the the mba rank from data.json, which will change whe  the fish type is changed
 // fishSelect.on('change', getMba);
 
-function getFishes(index, value()) {
+function getFishes(index, value) {
   $.ajax({
     type: "GET",
     url: "/datas",
@@ -19,7 +19,13 @@ function getFishes(index, value()) {
       console.log(data);
     }
   })
+  .done(function(response) {
+    var fish = response[0];
+    var fishTemplate = '<h1>' + fish.name + '</h1>'; 
+  })
+
 }
+
 
 // click event
 $(".fishname").on('change', function(){
@@ -30,6 +36,7 @@ $(".fishname").on('change', function(){
     dataType: "JSON",
     success: function (data){
       console.log(data);
+
     }
   })
   getFishes()
@@ -37,7 +44,7 @@ $(".fishname").on('change', function(){
 })
 
 
-results.html()
+// results.html()
 
 // end of document.ready
 });
